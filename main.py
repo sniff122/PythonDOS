33d06ae21bbada75b22e1441409ea1223ba1897c
NE1W01F <charliewolfDavid@gmail.com>
2019-07-21 13:25:33 +0000

    a

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def parser(string):
             arg = str(' '.join(string))
             print(str(arg[len(string[0]) + 1:]))
         else:
-            print("'{}' is not recognized as an internal or externel command,\noperable program or batch file\n".format(str(string[0])))
+            print("'{}' is not recognized as an internal or externel command, operable program or batch file\n".format(str(string[0])))
     except:
         pass
     
