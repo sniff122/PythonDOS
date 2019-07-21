19b01e4c87f134bb6d510b733088be102af71997
NE1W01F <charliewolfDavid@gmail.com>
2019-07-21 12:20:20 +0000

    removed cd and added echo

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def main():
 def parser(string):
     try:
         string = str(string).split(" ")
-        if string[0] == 'cd':
+        if string[0] == 'echo':
             string = str(''.join(string))
-            print(str(string[2:]))
+            print(str(string[4:]))
     except:
         pass
     
